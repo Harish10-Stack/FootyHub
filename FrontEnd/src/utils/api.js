6ba483e27c895217e4a91f04fe680afc95b8a8c7
Harish10-Stack <harishkaladharan10@gmail.com>
2026-01-26 13:24:25 +0530

    Update api.js

--- FrontEnd/src/utils/api.js
+++ FrontEnd/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: "https://footyhub-backend-hrqm.onrender.com",
   withCredentials: true, // ✅ REQUIRED for cookies
 });
 
