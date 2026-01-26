6bffe12b9f6db4e65b2e2bea794d8d63c8a547ef
Harish10-Stack <harishkaladharan10@gmail.com>
2026-01-26 14:33:43 +0530

    Change socket connection URL to production server

--- FrontEnd/src/utils/socket.js
+++ FrontEnd/src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io("https://footyhub-backend-hrqm.onrender.com", {
   withCredentials: true,
   autoConnect: true,
   transports: ['websocket', 'polling'], // Try websocket first, fallback to polling
